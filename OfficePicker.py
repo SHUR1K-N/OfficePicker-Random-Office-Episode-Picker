6c7593f3275cf9c296c59ebdfc4e7367d1ebfe4e
Aftab 'SHUR1KEN' Alam <42811989+SHUR1K-N@users.noreply.github.com>
2020-08-04 16:35:40 +0530

    Added dunder variable for the sake of convention/safety, and bobviously ecause Dunder Mifflin (doi)

--- OfficePicker.py
+++ OfficePicker.py
@@ -1,31 +1,32 @@
 import random; import os
 
-reRoll = True
+if __name__ == "__main__":
 
-os.system("@echo off")
+    reRoll = True
 
-while (reRoll is True):
+    os.system("@echo off")
 
-    os.system("cls")
+    while (reRoll is True):
 
-    randmS = random.randint(1, 9)
-    if (randmS == 1):
-        randmE = random.randint(1, 6)
-    elif (randmS) == 2:
-        randmE = random.randint(1, 22)
-    elif (randmS) == 3:
-        randmE = random.randint(1, 25)
-    elif (randmS) == 4:
-        randmE = random.randint(1, 19)
-    elif (randmS) == 5:
-        randmE = random.randint(1, 28)
-    elif (randmS) == 6:
-        randmE = random.randint(1, 26)
-    elif (randmS) == 7 or 9:
-        randmE = random.randint(1, 27)
-    elif (randmS) == 8:
-        randmE = random.randint(1, 24)
+        os.system("cls")
 
-    print("Season %d, Episode %d\n" % (randmS, randmE))
-    input("Press Enter to reroll.\n")
+        randmS = random.randint(1, 9)
+        if (randmS == 1):
+            randmE = random.randint(1, 6)
+        elif (randmS) == 2:
+            randmE = random.randint(1, 22)
+        elif (randmS) == 3:
+            randmE = random.randint(1, 25)
+        elif (randmS) == 4:
+            randmE = random.randint(1, 19)
+        elif (randmS) == 5:
+            randmE = random.randint(1, 28)
+        elif (randmS) == 6:
+            randmE = random.randint(1, 26)
+        elif (randmS) == 7 or 9:
+            randmE = random.randint(1, 27)
+        elif (randmS) == 8:
+            randmE = random.randint(1, 24)
 
+        print("Season %d, Episode %d\n" % (randmS, randmE))
+        input("Press Enter to reroll.\n")
