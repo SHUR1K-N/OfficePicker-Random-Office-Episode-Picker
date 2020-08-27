e5fd5bc6765427f760f15e1d870bce0bb3371b15
Aftab 'SHUR1KEN' Alam <42811989+SHUR1K-N@users.noreply.github.com>
2020-08-27 14:24:30 +0530

    Increased readability for the sake of own sanity while going through a code-cleaning wave

--- OfficePicker.py
+++ OfficePicker.py
@@ -10,23 +10,23 @@ if __name__ == "__main__":
 
         os.system("cls")
 
-        randmS = random.randint(1, 9)
-        if (randmS == 1):
-            randmE = random.randint(1, 6)
-        elif (randmS) == 2:
-            randmE = random.randint(1, 22)
-        elif (randmS) == 3:
-            randmE = random.randint(1, 25)
-        elif (randmS) == 4:
-            randmE = random.randint(1, 19)
-        elif (randmS) == 5:
-            randmE = random.randint(1, 28)
-        elif (randmS) == 6:
-            randmE = random.randint(1, 26)
-        elif (randmS) == 7 or 9:
-            randmE = random.randint(1, 27)
-        elif (randmS) == 8:
-            randmE = random.randint(1, 24)
+        randSeason = random.randint(1, 9)
+        if (randSeason == 1):
+            randEpisode = random.randint(1, 6)
+        elif (randSeason) == 2:
+            randEpisode = random.randint(1, 22)
+        elif (randSeason) == 3:
+            randEpisode = random.randint(1, 25)
+        elif (randSeason) == 4:
+            randEpisode = random.randint(1, 19)
+        elif (randSeason) == 5:
+            randEpisode = random.randint(1, 28)
+        elif (randSeason) == 6:
+            randEpisode = random.randint(1, 26)
+        elif (randSeason) == 7 or 9:
+            randEpisode = random.randint(1, 27)
+        elif (randSeason) == 8:
+            randEpisode = random.randint(1, 24)
 
-        print("Season %d, Episode %d\n" % (randmS, randmE))
+        print(f"Season {randSeason}, Episode {randEpisode}\n")
         input("Press Enter to reroll.\n")
