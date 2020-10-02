efec31bda6de3687542770adec70a5e6c3eeee6b
Aftab 'SHUR1KEN' Alam <42811989+SHUR1K-N@users.noreply.github.com>
2020-10-03 01:57:10 +0530

    Added keyboard interrupt handling

--- OfficePicker.py
+++ OfficePicker.py
@@ -12,25 +12,34 @@ if __name__ == "__main__":
 
     while (True):
 
-        clrscr()
+        try:
 
-        randSeason = random.randint(1, 9)
-        if (randSeason == 1):
-            randEpisode = random.randint(1, 6)
-        elif (randSeason) == 2:
-            randEpisode = random.randint(1, 22)
-        elif (randSeason) == 3:
-            randEpisode = random.randint(1, 25)
-        elif (randSeason) == 4:
-            randEpisode = random.randint(1, 19)
-        elif (randSeason) == 5:
-            randEpisode = random.randint(1, 28)
-        elif (randSeason) == 6:
-            randEpisode = random.randint(1, 26)
-        elif (randSeason) == 7 or 9:
-            randEpisode = random.randint(1, 27)
-        elif (randSeason) == 8:
-            randEpisode = random.randint(1, 24)
+            clrscr()
 
-        print(f"Season {randSeason}, Episode {randEpisode}\n")
-        input("Press Enter to reroll.\n")
+            randSeason = random.randint(1, 9)
+            if (randSeason == 1):
+                randEpisode = random.randint(1, 6)
+            elif (randSeason) == 2:
+                randEpisode = random.randint(1, 22)
+            elif (randSeason) == 3:
+                randEpisode = random.randint(1, 25)
+            elif (randSeason) == 4:
+                randEpisode = random.randint(1, 19)
+            elif (randSeason) == 5:
+                randEpisode = random.randint(1, 28)
+            elif (randSeason) == 6:
+                randEpisode = random.randint(1, 26)
+            elif (randSeason) == 7 or 9:
+                randEpisode = random.randint(1, 27)
+            elif (randSeason) == 8:
+                randEpisode = random.randint(1, 24)
+
+            print(f"Season {randSeason}, Episode {randEpisode}\n")
+            input("Press Enter to reroll.\n")
+        except KeyboardInterrupt:
+            clrscr()
+            print("\nCTRL ^C\n\nWhy are you the way that you are?")
+            print("Honestly, every time I try to do something fun or exciting, you make it... NOT that way.")
+            print("I hate so much about the things that you choose to be.")
+            print("\nPress Enter to exit.")
+            input()
